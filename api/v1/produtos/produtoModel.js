2f413e1a7013194a86c12628fba3778af9fc40f5
rafael <felzinpb@gmail.com>
2024-11-25 23:31:29 -0300

    criação do modelo do produto

---
+++ api/v1/produtos/produtoModel.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+const database = require('../../../config/db');
+
+const Produto = database.sequelize.define('Produto', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: true,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    brand: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    codeBar: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    dimension: {
+        type: Sequelize.JSON,
+        allowNull: true,
+    },
+    dimension: {
+        type: Sequelize.JSON,
+        allowNull: true,
+    },
+    weight: {
+        type: Sequelize.JSON,
+        allowNull: true,
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+
+}, {
+    tableName: 'produto'
+});
+
+module.exports = {Produto}
